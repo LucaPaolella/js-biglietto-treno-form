82ab351a4ce6980d710b1bb3677750c57a827ad1
Your Name <you@example.com>
2023-02-22 18:16:19 +0100

    Inizio milestone2

--- js/main.js
+++ js/main.js
@@ -7,5 +7,18 @@ let create = document.getElementById("create");
 
 
 document.getElementById("create").addEventListener('click',
-function () {});
+function () {
+    console.log(age.value)
+    console.log(km.value)
+    let AgeValue = age.value;
+    let KmValue = km.value;
+    let price = 0.21 * KmValue;
+    if (AgeValue >= 65){
+        price = parseInt (KmValue * 0.084)
+    }
+    else if (AgeValue < 18){
+        price = parseInt (KmValue * 0.042)
+    }
+    console.log(price)
+});
 
