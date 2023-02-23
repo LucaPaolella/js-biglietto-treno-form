c6f69adbf36659adcaa8b17cb77534a543fc6b7d
Your Name <you@example.com>
2023-02-23 12:30:04 +0100

    parte della seconda milestone

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 'use strict';
 
+
+// let milestone1
 let inputName = document.getElementById("name");
 let age = document.getElementById("age");
 let km = document.getElementById("km");
 let create = document.getElementById("create");
-let cost = document.getElementById("cost");
+
+// let milestone2
+let PassengerName = document.getElementById("passenger-name");
 let TypeTicket = document.getElementById("type-ticket");
+let cost = document.getElementById("cost");
+let ilTuoBiglietto = document.querySelector(".il-tuo-biglietto");
+
+
 
 
 document.getElementById("create").addEventListener('click',
@@ -13,6 +21,7 @@ document.getElementById("create").addEventListener('click',
         /*console.log(age.value)
         console.log(km.value)*/
 
+        //valori e svolgimento milestone 1 
         let NameValue = inputName.value; 
         console.log(NameValue);
 
@@ -22,6 +31,7 @@ document.getElementById("create").addEventListener('click',
         let KmValue = Number(km.value);
         console.log(KmValue);
 
+        //calcolo prezzo
         let price = 0.21 * KmValue; /*sconto 40%*/ 
         if (AgeValue === "over") {
             price = price * 0.6
@@ -32,15 +42,16 @@ document.getElementById("create").addEventListener('click',
         price = price.toFixed(2)
         console.log/*(price.toFixed(2))*/;
 
-        
-        
+        //valori e svolgimento milestone 2
           
-
+        let PassengerNameValue = PassengerName.value;
         let CostValue = cost.value;
         let PriceValue = price.value;
         let TypeTicketValue = TypeTicket.value
         CostValue = PriceValue;
         console.log(price);
+
+        ilTuoBiglietto.classList.add("show")
 
         if (AgeValue = "Over 65") {
             TypeTicketValue = "Over 65";
