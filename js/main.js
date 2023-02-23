bbd88fa352199124d8eb7f956b982b05438afd7a
Your Name <you@example.com>
2023-02-23 10:59:22 +0100

    correzione milestone 2

--- js/main.js
+++ js/main.js
@@ -1,37 +1,58 @@
 'use strict';
 
+let inputName = document.getElementById("name");
 let age = document.getElementById("age");
 let km = document.getElementById("km");
 let create = document.getElementById("create");
-
-document.getElementById("create").addEventListener('click',
-function () {
-    console.log(age.value)
-    console.log(km.value)
-    let AgeValue = age.value;
-    let KmValue = km.value;
-    let price = 0.21 * KmValue;
-    if (AgeValue >= 65){
-        price = parseInt (KmValue * 0.084)
-    }
-    else if (AgeValue < 18){
-        price = parseInt (KmValue * 0.042)
-    }
-    console.log(price);
-});
-
 let cost = document.getElementById("cost");
 let TypeTicket = document.getElementById("type-ticket");
 
-let CostValue=cost.value;
-let PriceValue= price.value; 
 
-CostValue = PriceValue;
-console.log(price);
+document.getElementById("create").addEventListener('click',
+    function () {
+        /*console.log(age.value)
+        console.log(km.value)*/
 
-if (AgeValue >= 65){
-    TypeTicket.value = "Anziano"
-}
-else if (AgeValue < 18){
-    TypeTicket.value = "Minorenne"
-}
+        let NameValue = inputName.value; 
+        console.log(NameValue);
+
+        let AgeValue = age.value;
+        console.log(AgeValue);
+
+        let KmValue = Number(km.value);
+        console.log(KmValue);
+
+        let price = 0.21 * KmValue; /*sconto 40%*/ 
+        if (AgeValue === "over") {
+            price = price * 0.6
+        }
+        else if (AgeValue === "minorenne") { /*sconto 20%*/
+            price = price * 0.8
+        }
+        price = price.toFixed(2)
+        console.log/*(price.toFixed(2))*/;
+
+        
+        
+        
+
+        let CostValue = cost.value;
+        let PriceValue = price.value;
+        let TypeTicketValue = TypeTicket.value
+        CostValue = PriceValue;
+        console.log(price);
+
+        if (AgeValue = "Over 65") {
+            TypeTicketValue = "Over 65";
+        }
+        else if (AgeValue < "Maggiorenne") {
+            TypeTicketValue = "Minorenne";
+        }
+        console.log(TypeTicketValue)
+
+    });
+
+
+
+
+
